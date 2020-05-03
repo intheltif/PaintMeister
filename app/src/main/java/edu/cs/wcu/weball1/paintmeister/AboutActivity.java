da833dcf22e8f899aaed1ff4f170960fd55498f6
Evert <weball1@catamount.wcu.edu>
2020-05-02 23:03:39 -0400

    Added a back button to About App screen app bar that removes it from the backstack once pressed.

--- app/src/main/java/edu/cs/wcu/weball1/paintmeister/AboutActivity.java
+++ app/src/main/java/edu/cs/wcu/weball1/paintmeister/AboutActivity.java
@@ -1,8 +1,11 @@
 package edu.cs.wcu.weball1.paintmeister;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.drm.DrmStore;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
     }
-}
+
+    /**
+     * Allows us to select an item in our AppBar.
+     * @param item The item selected by the user.
+     * @return true to consume method for our own use, false otherwise.
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } // end if
+
+        return super.onOptionsItemSelected(item);
+
+    } // end onOptionsItemSelected
+
+} // end AboutActivity class
