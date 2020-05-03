f004b67175b345500c9394b563ac595e5b9d8067
Chris <chriswolfdesign@gmail.com>
2020-05-03 18:07:22 -0400

    Saving and loading now works with brush color and sizes!!!

--- app/src/main/java/edu/cs/wcu/weball1/paintmeister/CustomView.java
+++ app/src/main/java/edu/cs/wcu/weball1/paintmeister/CustomView.java
@@ -26,28 +26,44 @@ import java.util.List;
  *
  * @author Evert Ball
  * @author Chris Wolf
- *
  */
 public class CustomView extends View {
 
-    /** An array list of Stroke objects to draw lines on the canvas**/
+    /**
+     * An array list of Stroke objects to draw lines on the canvas
+     **/
     private List<Stroke> strokes;
 
-    /** A list of lines that make up the drawings on a canvas. */
+    /**
+     * A list of lines that make up the drawings on a canvas.
+     */
     private List<Path> lines;
 
     private SparseArray<Stroke> activeStrokes;
 
-    /**The path currently being drawn by the user**/
+    /**
+     * The path currently being drawn by the user
+     **/
     private Path activePath;
 
-    /**The paint that is currently used to draw**/
+    /**
+     * The paint that is currently used to draw
+     **/
     Paint currentPaint;
 
-    /** Holds the text data for saving/loading paintings */
+    /**
+     * Holds the text data for saving/loading paintings
+     */
     StringBuilder stringRepresentation;
 
-    /** The brush width */
+    /**
+     * Default brush size
+     */
+    private static final int DEFAULT_WIDTH = 6;
+
+    /**
+     * The brush width
+     */
     private int width;
 
     /**
@@ -57,17 +73,17 @@ public class CustomView extends View {
      * @param attrs
      */
     public CustomView(Context context, AttributeSet attrs) {
-        super(context,attrs);
+        super(context, attrs);
 
-        //this.setOnTouchListener(this);
+        width = DEFAULT_WIDTH;
 
-        width = 6;
-
+        // Set up the brush
         currentPaint = new Paint();
         currentPaint.setStyle(Paint.Style.STROKE);
         currentPaint.setStrokeWidth(width);
         currentPaint.setColor(Color.GREEN);
 
+        // Set up the list of strokes
         strokes = new ArrayList<>();
         activeStrokes = new SparseArray<>();
         setFocusable(true);
@@ -81,84 +97,90 @@ public class CustomView extends View {
      */
     @Override
     protected void onDraw(Canvas canvas) {
-
-        if(strokes != null) {
-            for(Stroke stroke : strokes) {
-                if(stroke != null) {
+        if (strokes != null) {
+            for (Stroke stroke : strokes) {
+                if (stroke != null) {
                     Path path = stroke.getPath();
                     Paint paint = stroke.getPaint();
-                    if(path != null && paint != null) {
+                    if (path != null && paint != null) {
                         canvas.drawPath(path, paint);
-                    }
-                }
-            }
-        }
+                    } // end inner-if
+                } // end outer-if
+            } // end for
+        } // end if
 
     } // end onDraw
 
     /**
      * When the users finger is placed on the screem
-     * @param x coordinate
-     * @param y The y coordinate.
+     *
+     * @param x  coordinate
+     * @param y  The y coordinate.
      * @param id The pointer id
      */
-    public void touchDown(float x, float y, int id){
+    public void touchDown(float x, float y, int id) {
+        // Set the new brush settings
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(width);
         paint.setColor(currentPaint.getColor());
 
+        // Set up the new stroke
         PointF pt = new PointF(x, y);
         Stroke stroke = new Stroke(paint);
         stroke.addPoint(pt);
 
+        // add to the current strokes
         activeStrokes.put(id, stroke);
         strokes.add(stroke);
 
-
-        this.stringRepresentation.append("# " + x + " " + y + " ");
-    }
+        // add textual data for saving and loading
+        this.stringRepresentation.append("# " + currentPaint.getColor() + " " + width + " ");
+        this.stringRepresentation.append(x + " " + y + " ");
+    } // end touchDown
 
     /**
      * When the users finger is dragged.
-     * @param x coordinate
-     * @param y The y coordinate.
+     *
+     * @param x  coordinate
+     * @param y  The y coordinate.
      * @param id The pointer id
      */
-    public void touchMove(float x, float y, int id){
-
+    public void touchMove(float x, float y, int id) {
+        // Get the stroke currently being drawn and add this point to it
         Stroke stroke = activeStrokes.get(id);
-        if(stroke != null) {
-            PointF pt = new PointF(x,y);
+        if (stroke != null) {
+            PointF pt = new PointF(x, y);
             stroke.addPoint(pt);
-        }
+        } // end if
 
+        // add this textual data for saving and loading
         this.stringRepresentation.append(x + " " + y + " ");
-    }
+    } // end touchMove
 
     /**
      * When the users finger is lifted.
+     *
      * @param x The x coordinate
      * @param y The y coordinate.
      */
-    public void touchUp(float x, float y){
+    public void touchUp(float x, float y) {
         activePath = null;
 
+        // add this textual data for saving and loading
         this.stringRepresentation.append(x + " " + y + "\n");
-    }
+    } // end touchUp
 
     /**
      * When the view is touched.
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         final int action = event.getActionMasked();
         final int pointerCount = event.getPointerCount();
         //Define drawable attributes.
         float x = event.getX();
         float y = event.getY();
-
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
@@ -166,25 +188,25 @@ public class CustomView extends View {
                 touchDown(x, y, event.getPointerId(0));
                 break;
             case MotionEvent.ACTION_MOVE:
-                for(int i = 0; i < pointerCount; i++) {
-                    touchMove(event.getX(i), event.getY(i), event.getPointerId(i) );
+                for (int i = 0; i < pointerCount; i++) {
+                    touchMove(event.getX(i), event.getY(i), event.getPointerId(i));
                 }
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
-                touchUp(x,y);
+                touchUp(x, y);
                 break;
         }//end switch
 
+        // force the screen to "redraw"
         invalidate();
-
-
         return true;
 
     } // end onDraw method
 
     /**
      * Provides the textual representation of this drawing
+     *
      * @return the textual representation of this drawing
      */
     public String getStringRepresentation() {
@@ -193,55 +215,70 @@ public class CustomView extends View {
 
     /**
      * Takes in a string (from a file) and parses it and converts it into a painting
+     *
      * @param fileText the text from the file that has a saved painting
      */
     //TODO: Update to use the Stroke class
     public void setDrawingFromString(String fileText) {
         // Save the text to be added onto later
         this.stringRepresentation = new StringBuilder(fileText);
+        this.strokes = new ArrayList<>();
+        Stroke activeStroke = null;
 
         // Split the text file into pieces for parsing
         String[] fileCommands = fileText.split("\\s+");
-        this.activePath = null;
-        this.lines = new ArrayList<>();
 
         int i = 0;
-        while(i < fileCommands.length) {
-            // if this is the start of a new path
+        while (i < fileCommands.length) {
             if (fileCommands[i].equals("#")) {
-                if (this.activePath != null) {
-                    lines.add(this.activePath);
+                // if this is not the first stroke, add the last stroke to our list
+                if (activeStroke != null) {
+                    this.strokes.add(activeStroke);
                 } // end inner-if
 
-                this.activePath = new Path();
+                // Get the brush stats for this stroke
+                Paint newPaint = new Paint();
+                i++;
+                newPaint.setColor(Integer.parseInt(fileCommands[i]));
+                i++;
+                newPaint.setStrokeWidth(Float.parseFloat(fileCommands[i]));
+                newPaint.setStyle(Paint.Style.STROKE);
+
+                // "lift the brush" for the next stroke
+                activeStroke = new Stroke(newPaint);
             } else {
                 float xCoor = Float.parseFloat(fileCommands[i]);
                 i++;
                 float yCoor = Float.parseFloat(fileCommands[i]);
 
-                // if this is the first "dot" in the line, move to it
-                // otherwise, draw a line to it
-                if (fileCommands[i - 2].equals("#")) {
-                    this.activePath.moveTo(xCoor, yCoor);
-                } else {
-                    this.activePath.lineTo(xCoor, yCoor);
-                } // end if-else
-            } // end outer-else
+                activeStroke.addPoint(new PointF(xCoor, yCoor));
+            } // end else
 
+            // increment to the next bit of data in the file
             i++;
         } // end while
+
+        // add the last stroke that was read in
+        this.strokes.add(activeStroke);
     } // end setDrawingFromString
+
     /**
      * Sets the color of the paint we draw on the canvas with.
+     *
      * @param color The color to change to.
      */
     public void setPaintColor(int color) {
         this.currentPaint.setColor(color);
     } // end setPaintColor method
 
+    /**
+     * Sets the width of the current brush stroke
+     *
+     * @param width the new width of the brush stroke
+     */
     public void setBrushWidth(int width) {
         this.width = width;
-    }
+    } // end setBrushWidth
 
 } // end CustomView class
 
